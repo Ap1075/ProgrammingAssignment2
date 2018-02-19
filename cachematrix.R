f2813bd01c06cbba80e4d9709c6487a4edf35aef
Ap1075 <armaanpuri17@gmail.com>
2018-02-19 17:57:42 +0530

    First commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+#2 functions created, one to set values and also get values from, other to check presence of inverse and 
+# perform inversion in case inverse isn't already present. The functions are makeCacheMatrix and cacheSolve respectively.
 
-## Write a short comment describing this function
+# makeCachematrix creates a special "matrix" object that can cache its inverse.
+#list elements perform the following:
+# 1. set the value of the matrix
+# 2. get the value of the matrix
+# 3. set the value of inverse of the matrix
+# 4. get the value of inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+     mat = NULL
+     setter <- function(y)
+     {
+          x<<-y
+          mat<<-NULL
+          
+     }
+     getter <- function() x
+     setterinv <- function(inv) mat<<- inv
+     getterinv<- function() mat
+     list(setter = setter, getter = getter, setterinv = setterinv, getterinv=getterinv)
 }
 
 
-## Write a short comment describing this function
+# cacheSolve Checks whether inverse already exists and returned, if inverse is not present, inverse in calculated, then displayed
 
 cacheSolve <- function(x, ...) {
+     #get matrix
+     mat <- x$getterinv()  #if present use pre-existing value
+     if (!is.null(mat)){
+          message("getting inverted matrix")
+          return(mat)
+     }
+     data<- x$getter()      # if null, calc inverse using solve  
+     mat <- solve(data,...)
+     x$setterinv(mat)
+     mat
+     
         ## Return a matrix that is the inverse of 'x'
 }
